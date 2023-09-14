6f2c072ae5861e8707064d8bfe34d16fc1afc090
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-09-14 16:37:32 +0200

    New slicemachine

---
+++ src/lib/slices/Intro/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  /** @type {import("@prismicio/client").Content.IntroSlice} */
+  export let slice;
+</script>
+
+<section
+  data-slice-type={slice.slice_type}
+  data-slice-variation={slice.variation}
+>
+<h1>{slice.primary.title}</h1>
+<p>{slice.primary.text}</p>
+</section>
